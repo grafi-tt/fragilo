56d8668bedc45ac21f2887568c04be53040e1207
grafi <grafi@grafi.jp>
2014-12-22 17:27:55 +0900

    shinchoku

--- particle.js
+++ particle.js
@@ -54,7 +54,9 @@ var ParticleMangerSupport = {
 
 function ParticleManager(stdlib, foreign, heap) {
 	"use asm";
-	var log = stdlib.Math.log;
+	var sqrt = stdlib.Math.sqrt;
+	var log  = stdlib.Math.log;
+	var exp  = stdlib.Math.exp;
 	var fHeap = new stblib.Float32Array(heap);
 
 	var particleN = 0;
@@ -65,8 +67,14 @@ function ParticleManager(stdlib, foreign, heap) {
 	var width = 0, height = 0;
 	var noMoved = 0;
 
+	var userMoveAccumTime = 0;
+	var userMoveThreshold = 10;
+	var userMoveSttX, userMoveSttY, userMoveEndX, userMoveEndY;
+
 	function init(size, w, h) {
-		size = size | 0;
+		size = size|0;
+		w = w|0, h = h|0;
+
 		particleCoordsOffset = 0;
 		particleSpeedsOffset = 8 * size | 0;
 		particleColorsOffset = 16 * size | 0;
@@ -74,13 +82,36 @@ function ParticleManager(stdlib, foreign, heap) {
 		height = h;
 	}
 
-	function updateParticleSpeeds() {
+	function updateParticleSpeeds(time) {
+		time = +time;
+
+		var px = 0.0, py = 0.0, qx = 0.0, qy = 0.0;
+		var pvx = 0.0, pvy = 0.0, qvx = 0.0, qvy = 0.0;
+		var ax = 0.0, ay = 0.0;
+
+		var userMoveTime = 0.0;
+		var userMoveFired = 0;
+		var sx = 0.0, sy = 0.0, tx = 0.0, ty = 0.0;
+		var stx = 0.0, sty = 0.0;
+		var stnx = 0.0, stny = 0.0, stabs = 0.0;
+		var stsp = 0.0, stpt = 0.0;
+		var distToUserMove = 0.0;
+		var scaleUserMove = 0.0;
+
 		var dx = 0.0, dy = 0.0;
 		var d2 = 0.0;
 		var resistance = 0.0;
 		var ParticleRadius2 = 0.0;
 
 		var i = 0, j = 0, n8 = 0, end = 0;
+
+		sx = +userMoveSttX, sy = +userMoveSttY, tx = +userMoveEndX, ty = +userMoveEndY;
+		stx = +(tx - sx), sty = +(ty - sy);
+		stabs = +sqrt(+(wx * wx) + +(wy * wy));
+		stnx = +(stx / stabs), stny = +(sty / stabs);
+		userMoveTime = userMoveAccumTime + time;
+		userMoveFired = stabs > userMoveThreshold2;
+		userMoveAccumTime = userMoveFired ? 0.0 : userMoveTime;
 
 		ParticleRadius2 = 500.0;
 		n8 = 8*partcleN|0;
@@ -90,6 +121,17 @@ function ParticleManager(stdlib, foreign, heap) {
 			py = fHeap[((i+1)|0)>>2];
 			pvx = fHeap[((i+particleSpeedsOffset)|0)>>2];
 			pvy = fHeap[((((i+1)|0)+particleSpeedsOffset)|0)>>2];
+
+			if (userMoveFired) {
+				stsp = +(stnx * +(px - sx)) + +(stny * +(py - sx));
+				stpt = +(stnx * +(tx - px)) + +(stny * +(ty - px));
+				if (+(stsp * stpt) >= 0.0) {
+					distToUserMove = +(-stny * +(px - sx)) + +(stnx * +(py - sx));
+					scaleUserMove = exp(-distToUserMove);
+					pvx = +(pvx + +(scaleUserMove * stx));
+					pvy = +(pvy + +(scaleUserMove * sty));
+				}
+			}
 
 			for (j = i+1; j < n8; j = j+8|0) {
 				qx = fHeap[j>>2];
@@ -119,17 +161,19 @@ function ParticleManager(stdlib, foreign, heap) {
 		}
 	}
 
-	function updateParticleCoords() {
+	function updateParticleCoords(time) {
+		time = +time;
 		var v;
 		var n8 = 0, noMoving = 1;
+
 		n8 = 8*partcleN|0;
 		for (i = 0; i < n8; i = i+8|0) {
 			x = fHeap[i>>2];
 			y = fHeap[((i+1)|0)>>2];
 			vx = fHeap[((i+particleSpeedsOffset)|0)>>2];
 			vy = fHeap[((((i+1)|0)+particleSpeedsOffset)|0)>>2];
-			fHeap[i>>2] = +(x + vx);
-			fHeap[((i+1)|0)>>2] = +(y + vy);
+			fHeap[i>>2] = +(x + +(time * vx));
+			fHeap[((i+1)|0)>>2] = +(y + +(time * vy));
 			noMoving = noMoving & (vx == 0.0 & vy == 0.0);
 		}
 		noMoved = noMoving;
@@ -138,7 +182,13 @@ function ParticleManager(stdlib, foreign, heap) {
 	function addParticles() {
 	}
 
-	function reportWind() {
+	function reportUserMove(sttX, sttY, endX, endY) {
+		sttX = sttX|0, sttY = sttY|0;
+		endX = endX|0, endY = endY|0;
+
+		if (sttX != userMoveEndX || sttY != userMoveEndY)
+			userMoveSttX = sttX, userMoveSttY = sttY;
+		userMoveEndX = endX, userMoveEndY = endY;
 		noMoved = 1;
 	}
 
@@ -146,7 +196,11 @@ function ParticleManager(stdlib, foreign, heap) {
 		return particleN|0;
 	}
 
-	function progressTime(time) {
+	function progressTime(timems) {
+		timems = timems|0;
+		var time = 0.0;
+
+		time = +timems * 0.001;
 		if (!noMoved) {
 			addParticles();
 			updateParticleSpeeds(time);
@@ -157,7 +211,7 @@ function ParticleManager(stdlib, foreign, heap) {
 
 	return {
 		init:               init,
-		reportWind:         reportWind,
+		reportUserMove:     reportUserMove,
 		getParticleCount:   getParticleCount,
 		progressTime:       progressTime,
 	}
